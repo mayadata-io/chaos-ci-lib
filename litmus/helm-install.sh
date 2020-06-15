0e3ffad87aa89f04ce3a2f2d82e167407d1d5832
UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>
2020-06-15 22:28:13 +0530

    feat(lib): fixing the variable path for lib in container kill (#8)
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- litmus/helm-install.sh
+++ litmus/helm-install.sh
@@ -19,7 +19,7 @@ helm search repo k8s-chaos
 #Install the kubernetes chaos experiments
 helm install k8s k8s-chaos/kubernetes-chaos --set image.litmus.pullPolicy=${IMAGE_PULL_POLICY} \
 --set image.litmus.repository=${EXPERIMENT_IMAGE} --set image.litmus.tag=${EXPERIMENT_IMAGE_TAG} \
---set image.pumba.repository=${CONTAINER_KILL_HELPER_IMAGE} --set pumba.libName=${LIB} \
+--set image.pumba.repository=${CONTAINER_KILL_HELPER_IMAGE} --set image.pumba.libName=${LIB} \
 --set image.pumba.tag=${CONTAINER_KILL_HELPER_TAG}  --namespace=${APP_NS}
 
 #Checking the installation 
