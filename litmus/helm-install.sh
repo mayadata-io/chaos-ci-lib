816d152f38410a35525d0ba771e6841745aa2dc7
Udit Gaurav <uditgaurav@gmail.com>
2020-06-15 20:24:50 +0530

    feat(lib): Add lib details for installing container-kill using chaos-helm
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- litmus/helm-install.sh
+++ litmus/helm-install.sh
@@ -7,6 +7,9 @@ APP_NS=${APP_NS:="default"}
 IMAGE_PULL_POLICY=${IMAGE_PULL_POLICY:="Always"}
 EXPERIMENT_IMAGE=${EXPERIMENT_IMAGE:="litmuschaos/ansible-runner"}
 EXPERIMENT_IMAGE_TAG=${EXPERIMENT_IMAGE_TAG:="1.4.1"}
+CONTAINER_KILL_HELPER_IMAGE=${CONTAINER_KILL_HELPER_IMAGE:="gaiaadm/pumba"}
+CONTAINER_KILL_HELPER_TAG=${CONTAINER_KILL_HELPER_TAG:="0.6.5"}
+LIB=${LIB:="pumba"}
 
 #Add chaos helm repository
 helm repo add k8s-chaos https://litmuschaos.github.io/chaos-helm/
@@ -14,7 +17,10 @@ helm repo list
 helm search repo k8s-chaos
 
 #Install the kubernetes chaos experiments
-helm install k8s k8s-chaos/kubernetes-chaos --set image.litmus.pullPolicy=${IMAGE_PULL_POLICY} --set image.litmus.repository=${EXPERIMENT_IMAGE} --set image.litmus.tag=${EXPERIMENT_IMAGE_TAG} --namespace=${APP_NS}
+helm install k8s k8s-chaos/kubernetes-chaos --set image.litmus.pullPolicy=${IMAGE_PULL_POLICY} \
+--set image.litmus.repository=${EXPERIMENT_IMAGE} --set image.litmus.tag=${EXPERIMENT_IMAGE_TAG} \
+--set image.pumba.repository=${CONTAINER_KILL_HELPER_IMAGE} --set pumba.libName=${LIB} \
+--set image.pumba.tag=${CONTAINER_KILL_HELPER_TAG}  --namespace=${APP_NS}
 
 #Checking the installation 
 kubectl get chaosexperiments -n ${APP_NS}
